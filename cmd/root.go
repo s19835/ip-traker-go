2fe2565ca9bf59816d11a63e83f5ded71859be20
s19835 <s19835@sci.pdn.ac.lk>
2024-09-17 15:46:54 +0530

    Add: root descriptions

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,5 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2024 MARIA NIRANJAN <s19835@sci.pdn.ac.lk>
 */
 package cmd
 
@@ -10,20 +9,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ip-traker-go",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Short: "Trace the geographical location and other details of an IP address.",
+	Long:  `This CLI application allows users to trace the geographical location, ISP, and other details of a given IP address. It provides essential information such as the country, city, and latitude/longitude coordinates. Additionally, the tool can be used to analyze multiple IP addresses at once, making it a useful utility for network administrators, cybersecurity analysts, and general users who want to gain insights into IP address origins. It supports both IPv4 and IPv6 formats, and can also perform reverse lookups if necessary.`,
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
@@ -47,5 +37,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
